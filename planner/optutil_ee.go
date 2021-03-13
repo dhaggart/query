888c7cfc137c2ee999da0bae75855239ea6c7e95
dhaggart <donald.haggart@couchbase.com>
2021-03-13 18:50:02 +0000

    Revert MB-44770

--- planner/optutil_ee.go
+++ planner/optutil_ee.go
@@ -64,11 +64,6 @@ func optMinCost() float64 {
 	return optutil.MinCost()
 }
 
-func optCheckRangeExprs(baseKeyspaces map[string]*base.BaseKeyspace, advisorValidate bool,
-	context *PrepareContext) {
-	optutil.CheckRangeExprs(baseKeyspaces, advisorValidate, context)
-}
-
 func primaryIndexScanCost(primary datastore.PrimaryIndex, requestId string, context *PrepareContext) (
 	float64, float64, int64, float64) {
 	return optutil.CalcPrimaryIndexScanCost(primary, requestId, context)
